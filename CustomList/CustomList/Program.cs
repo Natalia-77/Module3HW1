d50fccb12e058d830f2002b5599fa204b4729143
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-12-05 13:08:01 +0200

    Create repo

---
+++ CustomList/CustomList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
